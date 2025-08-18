be75605b23bc252dfe3bb1027b8c1de434900b02
Lesedi Lengosane <tshwanelolengosane@gmail.com>
2025-08-18 19:38:05 +0200

    fixing a minor bug in routing.ts

--- src/app/auth/signin-callback/page.tsx
+++ src/app/auth/signin-callback/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter} from "next/navigation";
 import { supabase } from "../../api/DatabaseApi/supabaseClient";
 
 export default function SignInCallback() {
