f29afee3af2e324c2f4cb7db43d390536abe4ddf
Phutheho Mtloung <Mtloungphutheho@gmail.com>
2025-09-17 12:47:05 +0200

    the backend fetches lineups well and returns them

--- src/app/api/analyst/[matchid]/route.ts
+++ src/app/api/analyst/[matchid]/route.ts
@@ -15,7 +15,7 @@ export async function GET(request:Request,{params}:{
     
 try {
     //we need to fetch all both teamids then fetch all the default lineups or lineups set
-     const {data: teamids, error } = await supabase
+     const {data: teamids, error:matchError } = await supabase
     .from("matches")
     .select("away_team_id, home_team_id")
     .eq("match_id", matchid)
@@ -24,12 +24,47 @@ try {
     const awayTeamId = teamids?.away_team_id;
     const homeTeamId = teamids?.home_team_id;
 
-    if (error){
-        return NextResponse.json({error: error.message},{status:500})
+    if (matchError){
+        return NextResponse.json({error:matchError.message},{status:500})
     }
-
-    return NextResponse.json(teamids);
+    
+    //The above works well and returns the team ids
+    
     //Lineups are the team_players
+    // 2. Fetch lineups for both teams in the match
+    const { data: lineups, error: lineupError } = await supabase
+    .from("lineups")
+  .select(`
+    lineup_id,
+    match_id,
+    team_id,
+    player_id,
+    position,
+    is_starting,
+    jersey_number,
+    player:players (
+      first_name,
+      last_name,
+      player_id,
+      team_id
+    )
+  `)
+  .eq("match_id", matchid)
+  .in("team_id", [awayTeamId, homeTeamId]);
+
+    if (lineupError) {
+         return NextResponse.json({error :lineupError.message},{status :500})
+        }
+
+/**
+ * This is optional for splitting Your lineups
+ const awayLineup = lineups.filter((l) => l.teamid === away_team_id);
+const homeLineup = lineups.filter((l) => l.teamid === home_team_id);
+ */
+        return NextResponse.json(lineups)
+
+
+
     //The previous games summary data 
     
 } catch (err) {
