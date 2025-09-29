434274b7fa925bf451257943486cf4dce00a01a1
Jim Kalake <jimkalake@gmail.com>
2025-09-30 00:15:06 +0200

    updated some setting for CI/CD

--- jest.config.js
+++ jest.config.js
@@ -52,10 +52,10 @@ const customJestConfig = {
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      branches: 15,
-      functions: 15,
-      lines: 15,
-      statements: 15,
+      branches: 75,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 }
