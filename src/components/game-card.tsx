eaaa730f918cc971407faa18fb6be03ff96874a3
Phutheho Mtloung <Mtloungphutheho@gmail.com>
2025-10-19 16:10:26 +0200

    auto refresh after booking

--- src/components/game-card.tsx
+++ src/components/game-card.tsx
@@ -15,6 +15,8 @@ import {
 } from "react";
 import { supabase } from "../app/api/DatabaseApi/supabaseClient";
 import { BookingApiClient } from "../app/utils/BookGames";
+import { useMatches } from "@/app/context/MatchesContext";
+import { toast } from "sonner";
 
 /* -------------------------------------------------------------
    🧠 Context-based user cache (prevents duplicate Supabase calls)
@@ -247,6 +249,7 @@ const AnalystGameCard = memo(
     const { id: analystId } = useUserData();
     const [isBooked, setIsBooked] = useState(booked);
     const [booking, setBooking] = useState(false);
+    const {triggerRefetch}=useMatches()
 
     const handleBookClick = useCallback(
       async (e: React.MouseEvent) => {
@@ -258,6 +261,12 @@ const AnalystGameCard = memo(
           const result = await BookingApiClient.bookGame(match_id, analystId);
           if (result.success) {
             setIsBooked(true);
+            triggerRefetch();
+            toast.success("Game Booked Successfully.", {
+        description: "Refreshing...",
+      });
+        
+
           }
           alert(result.message || "Booking status updated.");
         } catch {
